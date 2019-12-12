c1a42fcc57d3a3f35961eb4a46eae05c271c94e6
yura boytsov <yura-boytsov@mail.ru>
2019-12-12 09:24:59 +0300

    Закачка задачи по дз

---
+++ Программа на С#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Введите n");
+                int n = int.Parse(Console.ReadLine());
+                double sum = 1;
+                for (int i = 1; i < n; i++)
+                {
+                    sum = sum + 1 / i;
+                }
+                Console.Write("Сумма ряда равна:{0}", sum);
+            Console.ReadKey();
+        
+        }
+    }
+}
